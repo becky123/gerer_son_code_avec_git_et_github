dea9bc118a282c3b0ba0004d13fa10d4f9f96a92
becky012 <rebeca.nenciu@gmail.com>
2015-09-23 11:04:37 +0200

    Creation page 404 interface

---
+++ 404.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found)
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+
+get_header(); 
+if($_SESSION['accounts']!=null) $text='Merci de bla bla bla.';
+else $text="Vous n'avez pas bli bla blu.";
+?>
+
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+
+			<article id="post-0" class="post error404 no-results not-found">
+				<header class="entry-header">
+					<h1 class="entry-title">Ooops! Une erreur est survenu !</h1>
+                    <h2><?php echo $text;?></h2>
+				</header>
+
+				<div class="entry-content">
+				</div><!-- .entry-content -->
+			</article><!-- #post-0 -->
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
